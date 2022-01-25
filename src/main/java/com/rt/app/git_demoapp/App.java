2dd0bfd881a2fa7d2b6dc0f2e5ec19bffebeabc6
Rta Diaz <rtadiaz552@gmail.com>
2022-01-25 13:01:38 +0530

    added if condition to check the data

--- src/main/java/com/rt/app/git_demoapp/App.java
+++ src/main/java/com/rt/app/git_demoapp/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addData(String data){
 		System.out.println("data"+ data);
+		if(data.equals("Rta Diaz")){
+			System.out.println("welcome");
+		}
 	}
 	
     public static void main( String[] args )
